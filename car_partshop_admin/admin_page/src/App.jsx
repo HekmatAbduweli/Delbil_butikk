e7a71596b0e510b3667f046b84e722f00accf7ed
HekmatAbduweli <143352767+HekmatAbduweli@users.noreply.github.com>
2024-10-14 10:45:58 +0200

    first commit

---
+++ car_partshop_admin/admin_page/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Rudy skal selge bilen sin som delbil fordi... ja han vet hvorfor</h1>
+    </>
+  );
+}
+
+export default App
