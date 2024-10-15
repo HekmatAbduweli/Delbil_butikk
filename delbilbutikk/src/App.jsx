b8b90f9431bcacd7b332eccd03d1e1b149daff14
HekmatAbduweli <143352767+HekmatAbduweli@users.noreply.github.com>
2024-10-15 17:04:36 +0200

    re created

---
+++ delbilbutikk/src/App.jsx
@@ -0,0 +1,12 @@
+import './output.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline color-blue">Rudy skal selge delbil</h1>
+      <p className="text-green-500">rudy skal selge delbil</p>
+    </>
+  );
+}
+
+export default App
